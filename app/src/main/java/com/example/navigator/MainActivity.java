d8db1310590598cef362256eaa7209f3d8177cea
Manam Imran <manamimran4ever@gmail.com>
2022-02-10 12:09:58 +0500

    first commit

---
+++ app/src/main/java/com/example/navigator/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.navigator;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.bottomnavigation.BottomNavigationView.OnNavigationItemReselectedListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
