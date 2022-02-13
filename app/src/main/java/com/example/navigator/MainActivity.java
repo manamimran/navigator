ad0a94e031e083ea49d6240b08cd0bbaff04662f
Manam Imran <manamimran4ever@gmail.com>
2022-02-13 13:40:22 +0500

    first commit

--- app/src/main/java/com/example/navigator/MainActivity.java
+++ app/src/main/java/com/example/navigator/MainActivity.java
@@ -1,21 +1,55 @@
 package com.example.navigator;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 
 import android.os.Bundle;
 
+import com.example.navigator.databinding.ActivityMainBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.bottomnavigation.BottomNavigationView.OnNavigationItemReselectedListener;
 
+
 public class MainActivity extends AppCompatActivity {
+
+ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new Fragment2());            //set home fragmemt o home screen
 
+        binding.bottomNav.setOnItemSelectedListener(item -> {
 
+            switch(item.getItemId()){
+                case R.id.home:
+                    replaceFragment(new Fragment2());
+                    break;
+                case R.id.profile:
+                    replaceFragment(new Fragment1());
+                    break;
+                case R.id.settings:
+                    replaceFragment(new Fragment3());
+                    break;
+            }
+
+            return true;
+        });
+
+    }
+
+    private void replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.container,fragment);
+        fragmentTransaction.commit();
     }
 }
